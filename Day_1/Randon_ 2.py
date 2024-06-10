8b6bac3812b9e992c314eb1a4b1f6a4ba8a9278e
Luis Caceres <76120872+MITDEVPRO@users.noreply.github.com>
2024-06-10 09:36:50 -0400

    Update Randon_ 2.py
    ingreso de inputs

--- Day_1/Randon_ 2.py
+++ Day_1/Randon_ 2.py
@@ -1,9 +1,7 @@
 import random
 
 names=("James", "John", "Ben")
-num_items
-
-
+num_items=(2)
 names = names_string.split(", ")
 num_items = len(names)
 random_choice = random.randint(0, num_items - 1)
